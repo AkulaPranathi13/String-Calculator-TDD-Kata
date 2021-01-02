0b5765d52e004805c9e4990fe8e153d192f60aa3
Akula Pranathi <2017028@iiitdmj.ac.in>
2021-01-02 17:11:31 +0530

    add function handles string with new line

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -8,14 +8,14 @@ public class StringCalculator {
 			return 0;
 		else if (numbers.length() == 1)
 			return Integer.parseInt(numbers);
-		else if (numbers.contains(",")) {
-			String[] nums = numbers.split(",");
+		else {
+			String[] nums = numbers.split("[ ,\n]+");
 			int sum = 0;
 			for (int i = 0; i < nums.length; i++) {
 				sum += Integer.parseInt(nums[i]);
 			}
 			return sum;
 		}
-		return -1;
+
 	}
 }
